e6094a3e4ee5c89183cd022b8ef347f1bd900625
vegcom <189027736+vegcom@users.noreply.github.com>
2026-08-27 20:15:20 -0500

    refactor: enhance logging configuration and improve OS detection script

--- scripts/os_check.py
+++ scripts/os_check.py
@@ -1,12 +1,45 @@
 import sys
 import os
+import argparse
+import json
+
+try:
+  import logging.config
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  with open(f'{script_dir}/log_config.json', 'r', encoding="utf-8") as f:
+      log_conf = json.load(f)
+  logging.config.dictConfig(log_conf)
+except Exception as e:
+    import logging
+    log = logging.getLogger(__name__)
+    log.error(getattr(e, '__dict__', str(e)))
 
 def main():
-    # If the OS is Linux, exit with 0 (True for Match exec)
-    # If it is Windows ('nt'), exit with 1 (False for Match exec)
-    if os.name != 'nt':
+    parser = argparse.ArgumentParser(
+            description="cozy-ssh local session state mapper",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument("os",
+            help="Evaluate if system is a particular platform",
+            default="linux", choices=["linux", "windows", "win32", "win"])
+
+    log = logging.getLogger("os_check")
+
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        log.error("[+] Failed to evaluate arguments")
         sys.exit(0)
-    sys.exit(1)
+
+    _os = "win32" if args.os in ["win", "win32", "windows"] else "linux"
+
+    if sys.platform == _os:
+        log.debug(f"[+] 👍 localhost os is {_os}")
+        sys.exit(0)
+    else:
+        log.warning(f"[+] 🚫 localhost os is not {_os}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
